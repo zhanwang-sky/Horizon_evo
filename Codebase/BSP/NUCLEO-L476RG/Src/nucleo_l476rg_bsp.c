e32cff0ce063b703ff43cece26baf74e1eedf7b2
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2018-10-14 13:38:25 +0800

    Application GS BMP280 demo

--- Codebase/BSP/NUCLEO-L476RG/Src/nucleo_l476rg_bsp.c
+++ Codebase/BSP/NUCLEO-L476RG/Src/nucleo_l476rg_bsp.c
@@ -187,7 +187,7 @@ void BSP_DMA_Init(void) {
 
 void BSP_USART2_UART_Init(void) {
     huart2.Instance = USART2;
-    huart2.Init.BaudRate = 9600;
+    huart2.Init.BaudRate = 115200;
     huart2.Init.WordLength = UART_WORDLENGTH_8B;
     huart2.Init.StopBits = UART_STOPBITS_1;
     huart2.Init.Parity = UART_PARITY_NONE;
