fd2bb399e35060043e8bee29c08b41e5ab3b643c
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2020-01-29 20:01:29 +0800

    read gyro data from BMX055
        Add submodule BMG160_driver
        FreeRTOSConfig.h: Bump up timer task's priority in order to avoid losing event bits.

--- Codebase/Application/main.c
+++ Codebase/Application/main.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 
 #include "bma2x2.h"
+#include "bmg160.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -35,13 +36,21 @@
 #define BMP280_I2C_FD       (0)
 
 #define BMX055_ACC_DEV_ADDR (BMA2x2_I2C_ADDR1 << 1)
-#define BMX055_GYR_DEV_ADDR (0X68 << 1)
+#define BMX055_GYR_DEV_ADDR (BMG160_I2C_ADDR1 << 1)
 #define BMX055_MAG_DEV_ADDR (0x10 << 1)
 #define BMP280_DEV_ADDR     (0x76 << 1)
 
-#define BIT_BMA_XFER_CPLT   (1 << 0)
+#define BIT_BMA_XFER_CPLT   (1)
 #define BIT_BMA_ARMED       (1 << 1)
 #define BIT_BMA_ERROR       (1 << 2)
+#define BIT_BMG_XFER_CPLT   (1 << 3)
+#define BIT_BMG_ARMED       (1 << 4)
+#define BIT_BMG_ERROR       (1 << 5)
+
+#define BMX_POWERUP_MS      (50)
+#define BMX_ACC_TIMEOUT_MS  (10)
+#define BMX_GYR_TIMEOUT_MS  (3)
+#define BMX_DATA_STABLE_MS  (2 * BMX_ACC_TIMEOUT_MS)
 
 /* Typedef -------------------------------------------------------------------*/
 struct bmx_busStatus {
@@ -50,16 +59,25 @@ struct bmx_busStatus {
 };
 
 /* Global variables ----------------------------------------------------------*/
-EventGroupHandle_t gEvt_globalEvents;
-SemaphoreHandle_t gSem_bmaNewData;
+SemaphoreHandle_t gSem_bmaDataReady;
+SemaphoreHandle_t gSem_bmgNewData;
 SemaphoreHandle_t gSem_patternRecv;
+EventGroupHandle_t gEvt_globalEvents;
 
 /* bma2x2 */
 struct bma2x2_t g_bma2x2;
 struct bmx_busStatus g_bmaBusStatus = { BIT_BMA_XFER_CPLT };
-volatile uint8_t g_bmaRawData[BMA2x2_ACCEL_XYZ_DATA_SIZE];
+volatile u8 g_bmaRawData[BMA2x2_ACCEL_XYZ_DATA_SIZE];
 volatile int g_bmaArmed;
 volatile int g_bmaIntrCnt;
+
+/* bmg160 */
+struct bmg160_t g_bmg160;
+struct bmx_busStatus g_bmgBusStatus = { BIT_BMG_XFER_CPLT };
+volatile u8 g_bmgRawData[BMG160_XYZ_DATA_SIZE];
+volatile int g_bmgArmed;
+volatile int g_bmgIntrCnt;
+volatile int g_bmgIntrResponse;
 
 /* test */
 volatile int g_seq;
@@ -76,6 +94,8 @@ const char g_strBlob[] =
 /* Function prototypes -------------------------------------------------------*/
 s8 bma_i2c_write(u8, u8, u8*, u8);
 s8 bma_i2c_read(u8, u8, u8*, u8);
+s8 bmg_i2c_write(u8, u8, u8*, u8);
+s8 bmg_i2c_read(u8, u8, u8*, u8);
 void bmx_delay_msec(u32);
 int bmx_busXferCplt(union sigval, int);
 
@@ -83,16 +103,18 @@ int bmx_busXferCplt(union sigval, int);
 /* BMX055 i2c API */
 s8 bmx_i2c_xfer(struct al_i2c_aiocb *aiocb, int (*f)(struct al_i2c_aiocb*)) {
     if ((*f)(aiocb) < 0) {
-        return ERROR;
+        return C_BMA2x2_FAILURE;
     }
     xEventGroupWaitBits(gEvt_globalEvents,
-                        ((struct bmx_busStatus*)aiocb->aio_sigevent.sigev_value.sival_ptr)->event,
+                        ((struct bmx_busStatus*) aiocb->aio_sigevent.sigev_value.sival_ptr)->event,
                         pdTRUE,
                         pdTRUE,
                         portMAX_DELAY);
-    return (s8) ((struct bmx_busStatus*)aiocb->aio_sigevent.sigev_value.sival_ptr)->ec;
+    return (((struct bmx_busStatus*) aiocb->aio_sigevent.sigev_value.sival_ptr)->ec == 0) ?
+        C_BMA2x2_SUCCESS : C_BMA2x2_FAILURE;
 }
 
+/* BMA2x2 */
 inline s8 bma_i2c_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
     struct al_i2c_aiocb aiocb = {
         BMX055_I2C_FD,
@@ -123,12 +145,49 @@ inline s8 bma_i2c_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
     return bmx_i2c_xfer(&aiocb, al_i2c_aio_read);
 }
 
+/* BMG160 */
+inline s8 bmg_i2c_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
+    struct al_i2c_aiocb aiocb = {
+        BMX055_I2C_FD,
+        dev_addr,
+        reg_addr,
+        reg_data,
+        cnt,
+        AIO_FLAGNONE,
+        { }
+    };
+    aiocb.aio_sigevent.sigev_value.sival_ptr = &g_bmgBusStatus;
+    aiocb.aio_sigevent.sigev_notify_function = bmx_busXferCplt;
+    return bmx_i2c_xfer(&aiocb, al_i2c_aio_write);
+}
+
+inline s8 bmg_i2c_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
+    struct al_i2c_aiocb aiocb = {
+        BMX055_I2C_FD,
+        dev_addr,
+        reg_addr,
+        reg_data,
+        cnt,
+        AIO_FLAGNONE,
+        { }
+    };
+    aiocb.aio_sigevent.sigev_value.sival_ptr = &g_bmgBusStatus;
+    aiocb.aio_sigevent.sigev_notify_function = bmx_busXferCplt;
+    return bmx_i2c_xfer(&aiocb, al_i2c_aio_read);
+}
+
 inline void bmx_delay_msec(u32 msec) { vTaskDelay(pdMS_TO_TICKS(msec)); }
 
 void bma_rawDataNormalize(uint8_t *rawData, int16_t *normalData) {
     for (int i = 0; i < 3; i++) {
-        normalData[i] = (int16_t) ((rawData[2 *i] & BMA2x2_12_BIT_SHIFT) | (rawData[2 * i + 1] << BMA2x2_SHIFT_EIGHT_BITS));
+        normalData[i] = (int16_t) ((rawData[2 * i] & BMA2x2_12_BIT_SHIFT) | (rawData[2 * i + 1] << BMA2x2_SHIFT_EIGHT_BITS));
         normalData[i] >>= BMA2x2_SHIFT_FOUR_BITS;
+    }
+}
+
+void bmg_rawDataNormalize(uint8_t *rawData, int16_t *normalData) {
+    for (int i = 0; i < 3; i++) {
+        normalData[i] = (int16_t) (rawData[2 * i] | rawData[2 * i + 1] << 8);
     }
 }
 
@@ -176,19 +235,48 @@ int bmx_busXferCplt(union sigval sigev_value, int ec) {
     return (int) higherPriorityTaskWoken;
 }
 
-/* external interrupts (fd = 0) */
+/* external interrupts (INT0) */
 void al_exti_0_callback(void) {
     BaseType_t xHigherPriorityTaskWoken;
 
     if (g_bmaArmed) {
         ++g_bmaIntrCnt;
         xHigherPriorityTaskWoken = pdFALSE;
-        xSemaphoreGiveFromISR(gSem_bmaNewData, &xHigherPriorityTaskWoken);
+        xSemaphoreGiveFromISR(gSem_bmaDataReady, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
+
+/* external interrupts (INT1) */
+void al_exti_1_callback(void) {
+    BaseType_t xHigherPriorityTaskWoken;
+
+    if (g_bmgArmed) {
+        ++g_bmgIntrCnt;
+        xHigherPriorityTaskWoken = pdFALSE;
+        xSemaphoreGiveFromISR(gSem_bmgNewData, &xHigherPriorityTaskWoken);
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 }
 
 /* Threads */
+void thr_printBlob(void *pvParameters) {
+    struct al_uart_aiocb aiocb = {
+        1,
+        (void*) g_strBlob,
+        sizeof(g_strBlob),
+        AIO_FLAGNONE,
+        { }
+    };
+    aiocb.aio_sigevent.sigev_value.sival_int = 1;
+    aiocb.aio_sigevent.sigev_notify_function = uart1_msgSent;
+
+    while (1) {
+        xSemaphoreTake(gSem_patternRecv, portMAX_DELAY);
+        al_uart_aio_write(&aiocb);
+    }
+}
+
 void thr_accelCollector(void *pvParameters) {
     s8 bma_rc = 0;
 
@@ -199,45 +287,46 @@ void thr_accelCollector(void *pvParameters) {
     g_bma2x2.bus_read = bma_i2c_read;
     g_bma2x2.delay_msec = bmx_delay_msec;
 
-    /* wait to power up (10ms) */
-    vTaskDelay(pdMS_TO_TICKS(10));
+    /* wait to power up */
+    vTaskDelay(pdMS_TO_TICKS(BMX_POWERUP_MS));
 
     /* init */
-    bma_rc |= bma2x2_init(&g_bma2x2);
+    bma_rc = bma2x2_init(&g_bma2x2);
 
     /* soft reset */
-    bma_rc |= bma2x2_soft_rst();
-    vTaskDelay(pdMS_TO_TICKS(10));
+    //bma_rc |= bma2x2_soft_rst();
 
-    /* set power mode */
+    /* switch to normal mode (default) */
     //bma_rc |= bma2x2_set_power_mode(BMA2x2_MODE_NORMAL);
 
     /* set range */
-    bma_rc |= bma2x2_set_range(BMA2x2_RANGE_16G); // 0x0F
+    bma_rc |= bma2x2_set_range(BMA2x2_RANGE_8G); // 0x0F
 
     /* set bandwidth */
     bma_rc |= bma2x2_set_bw(BMA2x2_BW_62_50HZ); // 0x10
 
     /* configure interrupt(s) */
-    //bma_rc |= bma2x2_set_intr_output_type(BMA2x2_INTR1_OUTPUT, PUSS_PULL);
-    //bma_rc |= bma2x2_set_intr_level(BMA2x2_INTR1_LEVEL, ACTIVE_HIGH);
+    /* set INT1 output type as push pull */
+    bma_rc |= bma2x2_set_intr_output_type(BMA2x2_INTR1_OUTPUT, PUSS_PULL); // 0x20
+    /* set INT1 active level as active high */
+    bma_rc |= bma2x2_set_intr_level(BMA2x2_INTR1_LEVEL, ACTIVE_HIGH); // 0x20
+    /* map data ready interrupt to INT1 pin */
     bma_rc |= bma2x2_set_new_data(BMA2x2_INTR1_NEWDATA, INTR_ENABLE); // 0x1A
 
-    /* as per datasheet, wait for at least 10ms, then (re)enable intr */
-    vTaskDelay(pdMS_TO_TICKS(25));
+    // as per datasheet, it should wait for at least 10ms, then (re)enable intrrupt
+    vTaskDelay(pdMS_TO_TICKS(BMX_DATA_STABLE_MS));
 
-    /* enable interrupts */
+    /* enable data ready interrupt */
     bma_rc |= bma2x2_set_intr_enable(BMA2x2_DATA_ENABLE, INTR_ENABLE); // 0x17
 
     /*================*/
-    /* notify the main thread that the accelerometer has been armed */
-    /* it will start polling data once both gyro and accel are armed */
-    xEventGroupSetBits(gEvt_globalEvents,
-                       ((bma_rc == 0) ? BIT_BMA_ARMED : BIT_BMA_ERROR));
+    /* notify that the accelerometer has been armed */
+    /* the main thread will start polling data once both gyro and accel are armed */
+    xEventGroupSetBits(gEvt_globalEvents, (bma_rc == 0) ? BIT_BMA_ARMED : BIT_BMA_ERROR);
 
     /*================*/
     while (1) {
-        if (xSemaphoreTake(gSem_bmaNewData, pdMS_TO_TICKS(10)) == pdTRUE) {
+        if (xSemaphoreTake(gSem_bmaDataReady, pdMS_TO_TICKS(BMX_ACC_TIMEOUT_MS)) == pdTRUE) {
             struct al_i2c_aiocb aiocb = {
                 BMX055_I2C_FD,
                 BMX055_ACC_DEV_ADDR,
@@ -256,26 +345,78 @@ void thr_accelCollector(void *pvParameters) {
     }
 }
 
-void thr_printBlob(void *pvParameters) {
-    struct al_uart_aiocb aiocb = {
-        1,
-        (void*) g_strBlob,
-        sizeof(g_strBlob),
-        AIO_FLAGNONE,
-        { }
-    };
-    aiocb.aio_sigevent.sigev_value.sival_int = 1;
-    aiocb.aio_sigevent.sigev_notify_function = uart1_msgSent;
+void thr_gyroCollector(void *pvParameters) {
+    s8 bmg_rc = 0;
 
+    /*================*/
+    /* configure bma2x2 */
+    g_bmg160.dev_addr = BMX055_GYR_DEV_ADDR;
+    g_bmg160.bus_write = bmg_i2c_write;
+    g_bmg160.bus_read = bmg_i2c_read;
+    g_bmg160.delay_msec = bmx_delay_msec;
+
+    /* wait to power up */
+    vTaskDelay(pdMS_TO_TICKS(BMX_POWERUP_MS));
+
+    /* init */
+    bmg_rc = bmg160_init(&g_bmg160);
+
+    /* soft reset (will cause I2C bus error) */
+    //bmg_rc |= bmg160_set_soft_rst();
+
+    /* switch to normal mode (default) */
+    //bmg_rc |= bmg160_set_power_mode(BMG160_MODE_NORMAL);
+
+    /* set full scale range */
+    bmg_rc |= bmg160_set_range_reg(BMG160_RANGE_2000); // 0x0F
+
+    /* set bandwidth */
+    bmg_rc |= bmg160_set_bw(BMG160_BW_230_HZ); // 0x10
+
+    /* configure interrupt(s) */
+    /* set INT3 output type as push pull */
+    bmg_rc |= bmg160_set_intr_output_type(BMG160_INTR1, 0); // 0x16
+    /* set INT3 active level as active high */
+    bmg_rc |= bmg160_set_intr_level(BMG160_INTR1, 1); // 0x16
+    /* map new data interrupt to INT3 pin */
+    bmg_rc |= bmg160_set_intr_data(BMG160_INTR1_DATA, BMG160_ENABLE); // 0x18
+
+    // XXX delay?
+    vTaskDelay(pdMS_TO_TICKS(BMX_DATA_STABLE_MS));
+
+    /* enable new data interrupt */
+    bmg_rc |= bmg160_set_data_enable(BMG160_ENABLE); // 0x15
+
+    /*================*/
+    /* notify that the accelerometer has been armed */
+    xEventGroupSetBits(gEvt_globalEvents, (bmg_rc == 0) ? BIT_BMG_ARMED : BIT_BMG_ERROR);
+
+    /*================*/
     while (1) {
-        xSemaphoreTake(gSem_patternRecv, portMAX_DELAY);
-        al_uart_aio_write(&aiocb);
+        if (xSemaphoreTake(gSem_bmgNewData, pdMS_TO_TICKS(BMX_GYR_TIMEOUT_MS)) == pdTRUE) {
+            struct al_i2c_aiocb aiocb = {
+                BMX055_I2C_FD,
+                BMX055_GYR_DEV_ADDR,
+                BMG160_RATE_X_LSB_BIT__REG,
+                g_bmgRawData,
+                BMG160_ALL_DATA_FRAME_LENGTH,
+                AIO_FLAGNONE,
+                { }
+            };
+            aiocb.aio_sigevent.sigev_value.sival_ptr = &g_bmgBusStatus;
+            aiocb.aio_sigevent.sigev_notify_function = bmx_busXferCplt;
+            ++g_bmgIntrResponse;
+            al_i2c_aio_read(&aiocb);
+       } else {
+           xEventGroupSetBits(gEvt_globalEvents, BIT_BMG_ERROR);
+       }
     }
 }
 
 void thr_main(void *pvParameters) {
     static char msgBuf[81];
     int16_t accData[3];
+    int16_t gyrData[3];
     unsigned int seq;
     TickType_t lastWakeTime;
     struct al_uart_aiocb uart1_aiocb = {
@@ -295,18 +436,21 @@ void thr_main(void *pvParameters) {
     uart1_aiocb.aio_nbytes = snprintf(msgBuf, sizeof(msgBuf), "initializing...\r\n");
     al_uart_aio_write(&uart1_aiocb);
 
-    /* get ready */
+    // wait to init...
     xEventGroupWaitBits(gEvt_globalEvents,
-                        BIT_BMA_ARMED,
+                        BIT_BMA_ARMED | BIT_BMG_ARMED,
                         pdTRUE,
                         pdTRUE,
                         portMAX_DELAY);
     g_bmaArmed = 1;
+    g_bmgArmed = 1;
 
-    /* start loop */
+    // start the main loop
     lastWakeTime = xTaskGetTickCount();
     while (1) {
-        seq = g_seq++;
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000));
+        seq = ++g_seq;
+
         xEventGroupClearBits(gEvt_globalEvents, BIT_BMA_XFER_CPLT);
         xEventGroupWaitBits(gEvt_globalEvents,
                             BIT_BMA_XFER_CPLT,
@@ -314,11 +458,21 @@ void thr_main(void *pvParameters) {
                             pdTRUE,
                             portMAX_DELAY);
         bma_rawDataNormalize((uint8_t*) g_bmaRawData, accData);
+
+        xEventGroupClearBits(gEvt_globalEvents, BIT_BMG_XFER_CPLT);
+        xEventGroupWaitBits(gEvt_globalEvents,
+                            BIT_BMG_XFER_CPLT,
+                            pdTRUE,
+                            pdTRUE,
+                            portMAX_DELAY);
+        bmg_rawDataNormalize((uint8_t*) g_bmgRawData, gyrData);
+
         uart1_aiocb.aio_nbytes = snprintf(msgBuf, sizeof(msgBuf),
-            "[%d](%d) { %hd, %hd, %hd }\r\n",
-            seq, g_bmaIntrCnt, accData[0], accData[1], accData[2]);
+            "[%d] (%d)(%d,%d) [{%hd,%hd,%hd},{%hd,%hd,%hd}]\r\n",
+            seq, g_bmaIntrCnt, g_bmgIntrCnt, g_bmgIntrResponse,
+            accData[0], accData[1], accData[2],
+            gyrData[0], gyrData[1], gyrData[2]);
         al_uart_aio_write(&uart1_aiocb);
-        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000));
     }
 }
 
@@ -332,8 +486,9 @@ int main(void) {
     al_tim_dshot_init();
 
     /* Create semaphores */
+    gSem_bmaDataReady = xSemaphoreCreateBinary();
+    gSem_bmgNewData = xSemaphoreCreateBinary();
     gSem_patternRecv = xSemaphoreCreateBinary();
-    gSem_bmaNewData = xSemaphoreCreateBinary();
 
     /* Create events */
     gEvt_globalEvents = xEventGroupCreate();
@@ -346,18 +501,25 @@ int main(void) {
                 tskIDLE_PRIORITY + 1,
                 NULL);
 
-    xTaskCreate(thr_printBlob,
-                "thread to print strBlob",
-                configMINIMAL_STACK_SIZE,
-                NULL,
-                tskIDLE_PRIORITY + 2,
-                NULL);
-
     xTaskCreate(thr_accelCollector,
                 "thread to collect Accelerometer's data",
                 configMINIMAL_STACK_SIZE,
                 NULL,
                 tskIDLE_PRIORITY + 2,
+                NULL);
+
+    xTaskCreate(thr_gyroCollector,
+                "thread to collect Gyroscope's data",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                tskIDLE_PRIORITY + 2,
+                NULL);
+
+    xTaskCreate(thr_printBlob,
+                "thread to print strBlob",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                tskIDLE_PRIORITY + 3,
                 NULL);
 
     /* Start the scheduler. */
